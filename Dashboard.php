5a89b7a4ef4ac516f93da1de5684f082272dfa6c
Rob-Shnayder <rjs12h@my.fsu.edu>
2014-11-11 22:59:51 -0500

    Car quiz feature beginning
    -Found data for car quiz. Tables and data in new DB file
    -Put together some questions for the car quiz, still no back-end
    implementation.
    -Car quiz form still needs formatting

--- Dashboard.php
+++ Dashboard.php
@@ -24,11 +24,12 @@ function showhide(id){
     </div>
     <ul>
       <li><a name="dashboard" onclick="showhide('dashboard')" href="#dashboard">dashboard</a></li>
-      <li><a name="Inventory" onclick="showhide('Inventory')" href="#Inventory">Inventory</a></li>
       <li><a name="BuyCar" onclick="showhide('BuyCar')" href="#BuyCar">Buy Car</a></li>
       <li><a name="SellCar" onclick="showhide('SellCar')" href="#SellCar">Sell Car</a></li>
-      <li><a name="Search" onclick="showhide('Search')" href="#Search">Search Inventory</a></li>      
-      <li><a name="Settings" onclick="showhide('Settings')" href="#Settings">Settings</a></li>
+      <li><a name="Search" onclick="showhide('Search')" href="#Search">Search Inventory</a></li>    
+      <li><a name="Inventory" onclick="showhide('CarQuiz')" href="#CarQuiz">Car Quiz</a></li>      
+      <li><a name="Inventory" onclick="showhide('reports')" href="#Reports">Reports</a></li>  
+      <li><a name="Settings" onclick="showhide('Settings')" href="#Settings">Account Management</a></li>
     </ul> 
   </div>
 
@@ -53,19 +54,20 @@ function showhide(id){
       <div id="SellCar" class="contentso" style="display:none;">
             <?php include ('Forms/SellCar_Form.html'); ?> 
           </div> 
-
-      <div id="Inventory" class="contentso" style="display:none;">     
-        <h1>Inventory</h1>
-
-      </div>
-
       <div id="Search" class="contentso" style="display:none;">
         <?php include ('Forms/Search_Form.html'); ?>        
       </div>    
- 
+
+      <div id="CarQuiz" class="contentso" style="display:none;">   
+        <?php include ('Forms/CarQuiz_Form.html'); ?>
+      </div>
+
+      <div id="Reports" class="contentso" style="display:none;">     
+        <h1>Reports</h1>
+      </div>
 
       <div id="Settings" class="contentso" style="display:none;">     
-        <h1>Settings</h1>
+        <h1>Account Management</h1>
       </div>
 
     </div>
